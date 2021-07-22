4495c2fcc1755388aba08d72089bd2cb4f7fab6d
Erik <erik.solveson@gmail.com>
2021-07-21 20:39:27 -0600

    finished the mvp

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,33 @@
 export const ADD_ONE = "ADD_ONE";
-
+export const CLEAR_DISPLAY = "CLEAR_DISPLAY";
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
+export const MEMORY_PLUS = "MEMORY_PLUS";
+export const MEMORY_ADD = "MEMORY_ADD";
+export const MEMORY_CLEAR = "MEMORY_CLEAR";
 
 export const addOne = () => {
-    return({type:ADD_ONE});
-}
+  return { type: ADD_ONE };
+};
 
 export const applyNumber = (number) => {
-    return({type:APPLY_NUMBER, payload:number});
-}
+  return { type: APPLY_NUMBER, payload: number };
+};
+
+export const changeOperation = (operation) => {
+  return { type: CHANGE_OPERATION, payload: operation };
+};
+
+export const clearDisplay = () => {
+  return { type: CLEAR_DISPLAY };
+};
+
+export const memoryPlus = (memory) => {
+  return { type: MEMORY_PLUS };
+};
+export const memoryAdd = (memory) => {
+  return { type: MEMORY_ADD };
+};
+export const memoryClear = (memory) => {
+  return { type: MEMORY_CLEAR };
+};
